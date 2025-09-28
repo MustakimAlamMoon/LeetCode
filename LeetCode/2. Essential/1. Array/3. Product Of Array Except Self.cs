c4a3ee8268770e6a1e43a94654fb37319d3859b6
Mustakim Alam Moon <mustakimalammoon@gmail.com>
2025-09-28 10:18:27 +0600

    Added new solution for "Product of Array Except Self" problem

---
+++ LeetCode/2. Essential/1. Array/3. Product Of Array Except Self.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Essential.Array
+{
+    class ProductOfArrayExceptSelfClass
+    {
+        /// <summary>
+        /// Method-1: Single Array (Best)
+        /// TC: O(n)
+        /// SC: O(1) - AS: O(n)
+        /// </summary>
+
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            int[] prod = new int[nums.Length];
+            prod[0] = 1;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                prod[i] = prod[i - 1] * nums[i - 1];
+            }
+
+            int right = 1;
+
+            for (int i = nums.Length - 2; i >= 0; i--)
+            {
+                right = right * nums[i + 1];
+                prod[i] = prod[i] * right;
+            }
+
+            return prod;
+        }
+    }
+}
