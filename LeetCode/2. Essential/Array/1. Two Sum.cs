d357707e6d91a43183329f99354a46dd1c3aaf89
Mustakim Alam Moon <mustakimalammoon@gmail.com>
2025-09-28 07:03:25 +0600

    Add project files.

---
+++ LeetCode/2. Essential/Array/1. Two Sum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Essential
+{
+    class TwoSumClass
+    {
+        /// <summary>
+        /// Method-1: Dictionary (Best)
+        /// TC: O(n)
+        /// SC: O(n)
+        /// </summary>
+
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var map = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var complement = target - nums[i];
+
+                if (map.ContainsKey(complement))
+                {
+                    return new int[] { map[complement], i };
+                }
+                else if (!map.ContainsKey(nums[i]))
+                {
+                    map.Add(nums[i], i);
+                }
+            }
+
+            return new int[] { };
+        }
+    }
+}
