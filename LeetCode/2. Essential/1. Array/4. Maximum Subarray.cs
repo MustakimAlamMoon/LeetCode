58c0a64461a2ad2215e63ddc7da9e1938610226b
Mustakim Alam Moon <mustakimalammoon@gmail.com>
2025-09-28 12:07:28 +0600

    Add solution for "Maximum Subarray"

---
+++ LeetCode/2. Essential/1. Array/4. Maximum Subarray.cs
@@ -0,0 +1,26 @@
+﻿
+namespace LeetCode.Essential.Array
+{
+    class MaximumSubarrayClass
+    {
+        /// <summary>
+        /// Method-1: Optimized (Best)
+        /// TC: O(n)
+        /// SC: O(1)
+        /// </summary>
+
+        public int MaxSubArray(int[] nums)
+        {
+            int max = nums[0];
+            int cur = nums[0];
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                cur = Math.Max(cur + nums[i], nums[i]);
+                max = Math.Max(cur, max);
+            }
+
+            return max;
+        }
+    }
+}
