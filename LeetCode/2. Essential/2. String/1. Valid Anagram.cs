e04d283e940a4bdd3ec8a5095253a9d0d44838c2
Mustakim Alam Moon <mustakimalammoon@gmail.com>
2025-09-29 23:34:46 +0600

    Add solution for "Valid Anagram"

---
+++ LeetCode/2. Essential/2. String/1. Valid Anagram.cs
@@ -0,0 +1,47 @@
+﻿namespace LeetCode.Essential.String
+{
+    class ValidAnagramClass
+    {
+        /// <summary>
+        /// Method-1: Dictionary (Best)
+        /// TC: O(n)
+        /// SC: O(n)
+        /// </summary>
+
+        public bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length)
+            {
+                return false;
+            }
+
+            var d = new Dictionary<char, int>();
+
+            foreach (char c in s)
+            {
+                if (!d.ContainsKey(c))
+                {
+                    d.Add(c, 1);
+                }
+                else
+                {
+                    d[c] += 1;
+                }
+            }
+
+            foreach (char c in t)
+            {
+                if (!d.ContainsKey(c))
+                {
+                    return false;
+                }
+                else
+                {
+                    d[c] -= 1;
+                }
+            }
+
+            return d.All(d => d.Value == 0);
+        }
+    }
+}
