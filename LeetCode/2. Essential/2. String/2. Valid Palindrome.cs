38e27ff367efbe58b15b654af924a6861d359ecf
Mustakim Alam Moon <mustakimalammoon@gmail.com>
2025-09-30 19:09:55 +0600

    Add solution for "Valid Palindrome"

---
+++ LeetCode/2. Essential/2. String/2. Valid Palindrome.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCode.Essential.String
+{
+    class ValidPalindromeClass
+    {
+        /// <summary>
+        /// Method-1: Two Pointer (Best)
+        /// TC: O(n)
+        /// SC: O(1)
+        /// </summary>
+        
+        public bool IsPalindrome(string s)
+        {
+            var left = 0;
+            var right = s.Length - 1;
+
+            while (left < right)
+            {
+                if (!char.IsLetterOrDigit(s[left]))
+                {
+                    left++;
+                }
+                else if (!char.IsLetterOrDigit(s[right]))
+                {
+                    right--;
+                }
+                else
+                {
+                    if (char.ToLower(s[left]) != char.ToLower(s[right]))
+                    {
+                        return false;
+                    }
+
+                    left++;
+                    right--;
+                }
+            }
+
+            return true;
+        }
+    }
+}
