1a2baee86b3c89dbbed2268c1883d467dc1a4e92
Mustakim Alam Moon <mustakimalammoon@gmail.com>
2025-09-30 20:33:51 +0600

    Add solution for "Longest Substring Without Repeating Characters"

---
+++ LeetCode/2. Essential/2. String/3. Longest Substring Without Repeating Characters.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode.Essential.String
+{
+    class LongestSubstringWithoutRepeatingCharactersClass
+    {
+        /// <summary>
+        /// Method-1: HashSet & Two Pointer (Best)
+        /// TC: O(n)
+        /// SC: O(n)
+        /// </summary>
+
+        public int LengthOfLongestSubstring(string s)
+        {
+            var set = new HashSet<char>();
+
+            int max = 0;
+            int start = 0;
+            int seen = 0;
+
+            while (seen < s.Length)
+            {
+                char c = s[seen];
+
+                if (!set.Contains(c))
+                {
+                    set.Add(c);
+                    max = Math.Max(max, seen - start + 1);
+                    seen++;
+                }
+                else
+                {
+                    set.Remove(s[start]);
+                    start++;
+                }
+            }
+
+            return max;
+        }
+    }
+}
