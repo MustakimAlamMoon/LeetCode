5a4d3da70278182016ac846eb7cb74f0d4554e53
Mustakim Alam Moon <mustakimalammoon@gmail.com>
2025-10-01 00:01:39 +0600

    Add solution for "Ransom Note"

---
+++ Essential/3. Hash Table/1. Two Sum.cs
@@ -0,0 +1,32 @@
+﻿namespace Essential.HashTable
+{
+    class TwoSumClass
+    {
+        /// <summary>
+        /// Method-1: Dictionary (Best)
+        /// TC: O(n)
+        /// SC: O(n)
+        /// </summary>
+
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var map = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var complement = target - nums[i];
+
+                if (map.ContainsKey(complement))
+                {
+                    return new int[] { map[complement], i };
+                }
+                else if (!map.ContainsKey(nums[i]))
+                {
+                    map.Add(nums[i], i);
+                }
+            }
+
+            return new int[] {};
+        }
+    }
+}
---
+++ Essential/3. Hash Table/2. Ransom Note.cs
@@ -0,0 +1,47 @@
+﻿namespace Essential.HashTable
+{
+    class RansomNoteClass
+    {
+        /// <summary>
+        /// Method-1: Dictionary (Best)
+        /// TC: O(m + n)
+        /// SC: O(n)
+        /// </summary>
+
+        public bool CanConstruct(string ransomNote, string magazine)
+        {
+            if (ransomNote.Length > magazine.Length)
+            {
+                return false;
+            }
+
+            var d = new Dictionary<char, int>();
+
+            foreach (char c in magazine)
+            {
+                if (!d.ContainsKey(c))
+                {
+                    d.Add(c, 1);
+                }
+                else
+                {
+                    d[c] += 1;
+                }
+            }
+
+            foreach (char c in ransomNote)
+            {
+                if (!d.ContainsKey(c) || d[c] == 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    d[c] -= 1;
+                }
+            }
+
+            return true;
+        }
+    }
+}
