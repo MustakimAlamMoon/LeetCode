f404bafdc3ef83d4cd5e38e9233f3e3ee3758603
Mustakim Alam Moon <mustakimalammoon@gmail.com>
2025-10-02 08:10:20 +0600

    Add solution for "Binary Search"

---
+++ Essential/5. Sort & Search/1. Binary Search.cs
@@ -0,0 +1,37 @@
+﻿namespace Essential.SortSearch
+{
+    class BinarySearchClass
+    {
+        /// <summary>
+        /// Method-1: Binary Search (Best)
+        /// TC: O(log n)
+        /// SC: O(1)
+        /// </summary>
+
+        public int Search(int[] nums, int target)
+        {
+            var s = 0;
+            var e = nums.Length - 1;
+
+            while (s <= e)
+            {
+                var mid = s + (e - s) / 2;
+
+                if (nums[mid] == target)
+                {
+                    return mid;
+                }
+                else if (target < nums[mid])
+                {
+                    e = mid - 1;
+                }
+                else
+                {
+                    s = mid + 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
