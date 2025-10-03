eb445b9de1955845d6325561a1c65c2179debad1
Mustakim Alam Moon <mustakimalammoon@gmail.com>
2025-10-04 05:39:49 +0600

    Add solution for "Reverse Linked List"

---
+++ Essential/7. Linked List/1. Reverse Linked List.cs
@@ -0,0 +1,40 @@
+﻿namespace Essential.LinkedList
+{
+    class ReverseLinkedListClass
+    {
+        /// <summary>
+        /// Method-1: Iterative (Best)
+        /// TC: O(n)
+        /// SC: O(1)
+        /// </summary>
+
+
+        // Definition for singly-linked list.
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        public ListNode ReverseList(ListNode head)
+        {
+            ListNode prevNode = null;
+
+            while (head != null)
+            {
+                ListNode nextNode = head.next;
+                head.next = prevNode;
+                prevNode = head;
+                head = nextNode;
+            }
+
+            return prevNode;
+        }
+    }
+}
