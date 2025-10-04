137ba02c3bfe392938f4300f62fa8a6e098332ed
Mustakim Alam Moon <mustakimalammoon@gmail.com>
2025-10-04 06:30:53 +0600

    Add solution for "Linked List Cycle"

---
+++ Essential/7. Linked List/2. Linked List Cycle.cs
@@ -0,0 +1,41 @@
+﻿namespace Essential.LinkedList
+{
+    class LinkedListCycleClass
+    {
+        /// <summary>
+        /// Method-1: Iterative (Best)
+        /// TC: O(n)
+        /// SC: O(1)
+        /// </summary>
+
+
+        // Definition for singly-linked list.
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+
+            public ListNode(int x)
+            {
+                val = x;
+                next = null;
+            }
+        }
+
+        public bool HasCycle(ListNode head)
+        {
+            var slow = head;
+            var fast = head;
+
+            while (fast != null && fast.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+
+                if (fast == slow) return true;
+            }
+
+            return false;
+        }
+    }
+}
