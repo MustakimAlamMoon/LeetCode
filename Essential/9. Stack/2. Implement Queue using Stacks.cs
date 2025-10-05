35fbd4f1c13051b1e2e356d8780bf903ae14a36a
Mustakim Alam Moon <mustakimalammoon@gmail.com>
2025-10-05 06:48:38 +0600

    Add solution for "Implement Queue using Stacks"

---
+++ Essential/9. Stack/2. Implement Queue using Stacks.cs
@@ -0,0 +1,63 @@
+﻿namespace Essential.Stack
+{
+    class ImplementQueueUsingStacksClass
+    {
+        /// <summary>
+        /// Method-1: Two Stacks (Best)
+        /// TC: Amortized O(1)
+        /// SC: O(n)
+        /// </summary>
+
+        public class MyQueue
+        {
+            Stack<int> input = new Stack<int>();
+            Stack<int> output = new Stack<int>();
+
+            public MyQueue()
+            {
+            }
+
+            // TC: O(1)
+            // SC: O(n)
+
+            public void Push(int x)
+            {
+                input.Push(x);
+            }
+
+            // TC: O(1)
+            // SC: O(1)
+
+            public int Pop()
+            {
+                int x = Peek();
+                output.Pop();
+                return x;
+            }
+
+            // TC: Amortized O(1)
+            // SC: O(1)
+
+            public int Peek()
+            {
+                if (output.Count == 0)
+                {
+                    while (input.Any())
+                    {
+                        output.Push(input.Pop());
+                    }
+                }
+
+                return output.Peek();
+            }
+
+            // TC: O(1)
+            // SC: O(1)
+
+            public bool Empty()
+            {
+                return input.Count == 0 && output.Count == 0;
+            }
+        }
+    }
+}
