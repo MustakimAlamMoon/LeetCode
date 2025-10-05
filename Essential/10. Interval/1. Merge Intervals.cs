6979cc5822995bb87dd02e2ff7ac2b981b3f32a3
Mustakim Alam Moon <mustakimalammoon@gmail.com>
2025-10-05 08:29:46 +0600

    Update project structure

---
+++ Essential/10. Interval/1. Merge Intervals.cs
@@ -0,0 +1,41 @@
+﻿namespace Essential.Interval
+{
+    class MergeIntervalsClass
+    {
+        /// <summary>
+        /// Method-1: Sorting (Best)
+        /// TC: O(nlogn)
+        /// SC: O(n)
+        /// </summary>
+
+        public int[][] Merge(int[][] intervals)
+        {
+            if (intervals.Length <= 1)
+                return intervals;
+
+            // Sort intervals by ascending start time
+            System.Array.Sort(intervals, (a, b) => a[0].CompareTo(b[0]));
+
+            List<int[]> result = new List<int[]>();
+            int[] newInterval = intervals[0];
+            result.Add(newInterval);
+
+            foreach (int[] interval in intervals)
+            {
+                // Overlapping intervals: extend the end time
+                if (interval[0] <= newInterval[1])
+                {
+                    newInterval[1] = Math.Max(newInterval[1], interval[1]);
+                }
+                else
+                {
+                    // Disjoint interval: start a new merge group
+                    newInterval = interval;
+                    result.Add(newInterval);
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
