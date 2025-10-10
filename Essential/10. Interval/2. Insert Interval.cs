5ea4e02bedf0f8508ef07e830331acaa5696752b
Mustakim Alam Moon <mustakimalammoon@gmail.com>
2025-10-11 05:36:58 +0600

    Add solution for "Insert Interval"

---
+++ Essential/10. Interval/2. Insert Interval.cs
@@ -0,0 +1,37 @@
+﻿namespace Essential.Interval
+{
+    class InsertIntervalClass
+    {
+        /// <summary>
+        /// Method-1: Linear Search (Best)
+        /// TC: O(n)
+        /// SC: O(1)
+        /// </summary>
+
+        public int[][] Insert(int[][] intervals, int[] newInterval)
+        {
+            List<int[]> result = new List<int[]>();
+            int i = 0;
+
+            // Step 1 - add all intervals ending before newInterval starts
+            while (i < intervals.Length && intervals[i][1] < newInterval[0])
+                result.Add(intervals[i++]);
+
+            // Step 2 - update the newInterval by merging with all overlapping intervals
+            while (i < intervals.Length && intervals[i][0] <= newInterval[1])
+            {
+                newInterval[0] = Math.Min(newInterval[0], intervals[i][0]);
+                newInterval[1] = Math.Max(newInterval[1], intervals[i][1]);
+                i++;
+            }
+
+            result.Add(newInterval); // add updated interval
+
+            // Step 3 - add remaining intervals
+            while (i < intervals.Length)
+                result.Add(intervals[i++]);
+
+            return result.ToArray();
+        }
+    }
+}
