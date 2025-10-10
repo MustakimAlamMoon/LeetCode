19f1341338db848a5ce5776158978f26df201693
Mustakim Alam Moon <mustakimalammoon@gmail.com>
2025-10-11 05:52:02 +0600

    Add solution for "Maximum Depth of Binary Tree"

---
+++ Essential/11. Tree/1. Maximum Depth of Binary Tree.cs
@@ -0,0 +1,37 @@
+﻿namespace Essential.Tree
+{
+    class MaximumDepthOfBinaryTreeClass
+    {
+        /// <summary>
+        /// Method-1: Recursive DFS (Best)
+        /// TC: O(n)
+        /// SC: O(n)
+        /// </summary>
+
+
+        // Definition for a binary tree node.
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null) return 0;
+
+            int maxLeft = MaxDepth(root.left);
+            int maxRight = MaxDepth(root.right);
+
+            return Math.Max(maxLeft, maxRight) + 1;
+        }
+    }
+}
